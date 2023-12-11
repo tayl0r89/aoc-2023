8573b4a869368b640c28ffe6a20888d183501d4b
Steven Taylor <steven.taylor788@googlemail.com>
2023-12-11 14:00:06 +0000

    part 2, day 11

--- challenges/day11/solution.py
+++ challenges/day11/solution.py
@@ -1,37 +1,35 @@
 import math
 
-def expand(data):
-    additional_cols = []
+def expand_and_get_planets(data, expansion = 1):
+    col_expansion = []
+    row_expansion = []
     for y, row in enumerate(data):
         contains_planet = False
         for x, item in enumerate(data[y]):
             if data[x][y] == "#":
                 contains_planet = True
-        additional_cols.append(contains_planet)
-
-    expanded = []
-    for y, row in enumerate(data):
-        new_row = []
-        for x, item in enumerate(row):
-            if additional_cols[x] == False:
-                new_row.extend([".", "."])
-            else:
-                new_row.append(item)
-        expanded.append(new_row)
+        col_expansion.append(0 if contains_planet else expansion)
+        
         if list(set(row)) == ["."]:
-            expanded.append(new_row)
+            row_expansion.append(expansion)
+        else:
+            row_expansion.append(0)
     
-    return expanded
+    planets = []
+    for y, row in enumerate(data):
+        for x, item in enumerate(row):
+            if item == "#":
+                planets.append((x + sum(col_expansion[0:x]), y + sum(row_expansion[0:y])))
+    
+    return planets
 
 def get_distance(planet_a, planet_b) -> int:
     x_diff = abs(planet_a[0] - planet_b[0])
     y_diff = abs(planet_a[1] - planet_b[1])
 
     if x_diff == 0:
-        print(f"From {planet_a} to {planet_b} distance {y_diff}")
         return y_diff
     elif y_diff == 0:
-        print(f"From {planet_a} to {planet_b} distance {x_diff}")
         return x_diff
     
     distance = (y_diff * 2) + (x_diff - y_diff)
@@ -41,6 +39,17 @@ def get_distance(planet_a, planet_b) -> int:
     return distance
 
 
+def calculate_shortest_all_pairs(planets):
+    total_distance = 0
+    planet_pair_count = 0
+    for i, val in enumerate(planets):
+        for j in range(i + 1, len(planets)):
+            distance = get_distance((val), planets[j])
+            total_distance = total_distance + distance
+            planet_pair_count = planet_pair_count + 1
+    print(f"Total pair count is {planet_pair_count}")
+    return total_distance
+
 if __name__ == "__main__":
     x_offset = 0
     y_offset = 0
@@ -49,27 +58,19 @@ if __name__ == "__main__":
         for line in file.readlines():
             data.append([*line.strip()])
     
-    expanded = expand(data)
 
-    for i, val in enumerate(expanded):
-        print("".join(val))
+    print("===== PART 1 =======")
 
+    planets = expand_and_get_planets(data)
     
-    planets = []
-    for y, row in enumerate(expanded):
-        for x, item in enumerate(row):
-            if item == "#":
-                planets.append((x,y))
-    
-    total_distance = 0
-    planet_pair_count = 0
-    for i, val in enumerate(planets):
-        for j in range(i + 1, len(planets)):
-            distance = get_distance((val), planets[j])
-            total_distance = total_distance + distance
-            planet_pair_count = planet_pair_count + 1
-    
+    total_distance = calculate_shortest_all_pairs(planets)
     print(total_distance)
-    print(planet_pair_count)
 
+
+    print("===== PART 2 =======")
+
+    planets = expand_and_get_planets(data, expansion=999999)
+    
+    total_distance = calculate_shortest_all_pairs(planets)
+    print(total_distance)
     
